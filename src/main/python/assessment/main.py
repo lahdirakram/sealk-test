b8e0feb20a96df7dd4f7760d6b05210bfcc3b15e
Lahdir Mohamed Akram <akramdzbuisness@gmail.com>
2021-07-22 23:58:02 +0200

    initial commit

--- src/main/python/assessment/main.py
+++ src/main/python/assessment/main.py
@@ -2,6 +2,7 @@ import sys
 import click
 import logging
 from os import path, pardir
+import threading, queue, csv
 
 # Local imports
 sys.path.append(path.abspath(path.join(path.dirname(__file__), pardir)))
@@ -13,7 +14,7 @@ logging.basicConfig(format="%(message)s", level=logging.INFO)
 @click.command()
 @click.option(
     "--filename",
-    default="/tmp/nasdaq-company-list.csv",
+    default="nasdaq-company-list.csv",
     help="Input CSV file name"
 )
 @click.option(
@@ -21,14 +22,55 @@ logging.basicConfig(format="%(message)s", level=logging.INFO)
     default=50,
     help="Number of companies Symbols to print at the end"
 )
+
+@click.option(
+    "--threads",
+    default=8,
+    help="Number of threads to use"
+)
 def main(filename: str, threads: int, top=10) -> None:
     """
     Args:
         filename (str): Input CSV file name
         top (int, optional): Number of companies Symbols to print at the end. Defaults to 10.
     """
-    logging.info(getCompanyAttractiveness("GOOGL"))
+    fileContent = list(csv.DictReader(open(filename)))
+
+    for row in fileContent:
+        waitingQueue.put(row["Symbol"])
+
+    threadsList=[]
+    for _ in range(threads):
+        threadsList.append(threading.Thread(target=computingRunner))
+    for th in threadsList:
+        th.start()
+    for th in threadsList:
+        th.join()
+
+    sortedResults = sorted(
+        resultQueue.queue, 
+        key=lambda result: result["score"], 
+        reverse=True
+        )
+
+    toPrint = ", ".join(
+        [result["id"] for result in sortedResults[:top]]
+        )
+
+    logging.info(toPrint)
+
+def computingRunner():
+
+    global waitingQueue, resultQueue
+
+    while not waitingQueue.empty():
+        symbole = waitingQueue.get()
+        ca = getCompanyAttractiveness(symbole) 
+        resultQueue.put(ca)
 
 
 if __name__ == "__main__":
+
+    waitingQueue = queue.Queue()
+    resultQueue = queue.Queue()
     main()
