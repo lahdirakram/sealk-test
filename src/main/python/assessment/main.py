fa0e91873259fd11a5e807d97f5858b129aee16c
Mohamed Lahdir <mohamedakram.lahdir@gmail.com>
2021-07-23 17:12:59 +0200

    première version

--- src/main/python/assessment/main.py
+++ src/main/python/assessment/main.py
@@ -3,6 +3,11 @@ import click
 import logging
 from os import path, pardir
 
+# MultiProcessing imports
+import threading, multiprocessing
+import queue
+import csv
+
 # Local imports
 sys.path.append(path.abspath(path.join(path.dirname(__file__), pardir)))
 from common.dummy_ai import getCompanyAttractiveness
@@ -21,14 +26,74 @@ logging.basicConfig(format="%(message)s", level=logging.INFO)
     default=50,
     help="Number of companies Symbols to print at the end"
 )
+@click.option(
+    "--threads",
+    default=multiprocessing.cpu_count(),
+    help="Number of threads to use."
+)
 def main(filename: str, threads: int, top=10) -> None:
     """
     Args:
         filename (str): Input CSV file name
         top (int, optional): Number of companies Symbols to print at the end. Defaults to 10.
     """
-    logging.info(getCompanyAttractiveness("GOOGL"))
+    # loading data from csv file
+    data = list(csv.DictReader(open(filename)))
+
+    # puting data in waitingQueue to be processed
+    for row in data:
+        waitingQueue.put(row["Symbol"])
+
+    # creating threads
+    threadList=[]
+    for index in range(threads):
+        thread = threading.Thread(
+            target=runner, 
+            args=(
+                index,
+                getCompanyAttractiveness,
+                )
+            )
+        threadList.append(thread)
+
+    # starting threads
+    for thread in threadList:
+        thread.start()
+
+    # waiting for threads to finish processing data
+    for thread in threadList:
+        thread.join()
+    
+    # sorting data processing result by the score of Company Attractiveness in descending order
+    sortedResults = sorted(
+        resultQueue.queue, 
+        key=lambda result: result["score"], 
+        reverse=True
+        )
+    
+    # keeping top results only
+    topResults = sortedResults[:top]
+    
+    logging.info(topResults)
+    
+def runner(id, task):
+
+    global waitingQueue, resultQueue
+
+    print(f"runner {id} starting now")
+
+    while not waitingQueue.empty():
+        # pickup data
+        data = waitingQueue.get() 
+        # run task on data
+        result = task(data) 
+        # store task result
+        resultQueue.put(result) 
+    
+    print(f"runner {id} finished processing")
 
 
 if __name__ == "__main__":
+    waitingQueue = queue.Queue()
+    resultQueue = queue.Queue()
     main()
